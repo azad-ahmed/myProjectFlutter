a07f3943842a798d9d5a028b07554a100a32305c
Azad Ahmed <azad.ahmed@hftm.ch>
2024-02-23 17:40:13 +0100

    add build automation on Github

--- lib/common/widgets/bottom_bar.dart
+++ lib/common/widgets/bottom_bar.dart
@@ -47,7 +47,7 @@ class _BottomBarState extends State<BottomBar> {
             decoration: BoxDecoration(
                 border: Border(
                     top: BorderSide(
-              color: _page == 0
+              color: _page == 2
                   ? GlobalVariables.selectedNavBarColor
                   : GlobalVariables.backgroundColor,
               width: bottomBarBorderWidth,
